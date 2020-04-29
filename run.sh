298596ef2b5944eb566f02577be95df32ab3f86b
alfinsuryaS <benjaminstwo@gmail.com>
2020-04-29 22:18:13 +0800

    Add: babel for compile js, easy run using command line

---
+++ run.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+
+tab_name="$1"
+shift
+
+cmd=""
+cdto="$PWD"
+args="$@"
+
+if [ -d "$1" ]; then
+    cdto=$(cd "$1" && pwd)
+    args="${@:2}"
+fi
+
+if [ -n "$args" ]; then
+    cmd="; $args"
+fi
+
+osascript <<EOF
+    tell application "iTerm"
+        activate
+        tell current window
+            tell current session
+                write text "ng serve"
+            end tell
+            set newTab to (create tab with default profile)
+            tell current session of newTab
+                write text "cd \"$cdto\"$cmd && npm run dev:watch-sass"
+            end tell
+             set newTab to (create tab with default profile)
+            tell current session of newTab
+                write text "cd \"$cdto\"$cmd && npm run dev:watch-js"
+            end tell
+        end tell
+    end tell
+EOF
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
-console.log("Hello");
+function hello_world() {
+    return "hello angular!"
+}
