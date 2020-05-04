7a8e1108385951371c55e3ae2aee96ed61e011fd
alfinsuryaS <benjaminstwo@gmail.com>
2020-05-04 16:42:13 +0800

    Test commit pake emoji ❤️❤️❤️

---
+++ examples/react/src/app/app.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { hot } from "react-hot-loader";
+
+const App = () => (
+    <main className="app">
+       <h1>Hello React!</h1>
+       <salt-button value="I'm a Web Component"></salt-button>
+    </main>
+);
+
+declare let module: object;
+
+export default hot(module)(App);
