b1b28870584b208a6154d920e8a476b9a3620a03
unknown <yudigunawan52@gmail.com>
2020-05-05 19:36:55 +0700

    Change Webcomponent to library

--- compileEl.sh
+++ compileEl.sh
@@ -1,11 +1,11 @@
 
 #!/bin/bash
-search_dirs=($(ls projects/salt/api/webcomponent/app/components/))
+search_dirs=($(ls projects/salt/api/library/app/components/))
 rm -rf ./dist/salt/api/library/
 mkdir ./dist/salt/api/library
 
 for dir in ${search_dirs[*]}; do
-    ng run webcomponent:build:production --aot --output-path=tmp/${dir} --main="projects/salt/api/webcomponent/app/components/${dir}/compile.ts"
+    ng run library:build:production --aot --output-path=tmp/${dir} --main="projects/salt/api/library/app/components/${dir}/compile.ts"
     cat ./tmp/${dir}/{runtime,polyfills,main}-es5.js > ./tmp/${dir}.js
     cp -r ./tmp/${dir}.js ./dist/salt/api/library
     rm -rf ./tmp/
